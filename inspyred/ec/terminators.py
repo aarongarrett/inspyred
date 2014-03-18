65625381b24855bebbad7424faa98241ff9ec2d7
Aaron Garrett <aaron.lee.garrett@gmail.com>
2014-03-18 09:56:37 -0500

    no real changes

--- inspyred/ec/terminators.py
+++ inspyred/ec/terminators.py
@@ -275,3 +275,37 @@ def user_termination(population, num_generations, num_evaluations, args):
             count += 1
     sys.stdout.write('\n')
     return False    
+
+    
+def no_improvement_termination(population, num_generations, num_evaluations, args):
+    """Return True if the best fitness does not change for a number of generations.
+    
+    This function keeps track of the current best fitness and compares it to
+    the best fitness in previous generations. Whenever those values are the 
+    same, it begins a generation count. If that count exceeds a specified 
+    number, the terminator returns True.
+    
+    .. Arguments:
+       population -- the population of Individuals
+       num_generations -- the number of elapsed generations
+       num_evaluations -- the number of candidate solution evaluations
+       args -- a dictionary of keyword arguments
+    
+    Optional keyword arguments in args:
+    
+    - *max_generations* -- the number of generations allowed for no change in fitness (default 10)
+    
+    """
+    max_generations = args.setdefault('max_generations', 10)
+    previous_best = args.setdefault('previous_best', None)
+    current_best = max(population).fitness
+    if previous_best is None or previous_best != current_best:
+        args['previous_best'] = current_best
+        args['generation_count'] = 0
+        return False
+    else:
+        if args['generation_count'] >= max_generations:
+            return True
+        else:
+            args['generation_count'] += 1
+            return False
